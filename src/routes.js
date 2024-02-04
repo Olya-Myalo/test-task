0e63a0f368f46952fc4a69d08d13be1c5ec09d9d
Olya-Myalo <olyamyalo@mail.ru>
2024-02-04 21:53:04 +0300

    добавлено NotFound

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import UserSearch from './components/UserSearch/UserSearch'
 import ItemUser from './components/ItemUser/ItemUser'
+import { NotFound } from './components/notFoundPage'
 
 const AppRoutes = () => {
   return (
@@ -8,6 +9,7 @@ const AppRoutes = () => {
       <Routes>
         <Route path="/" element={<UserSearch />} />
         <Route path="/:UserId" element={<ItemUser />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
   )
